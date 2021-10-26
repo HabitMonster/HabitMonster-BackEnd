12ccf914d2b9a3da19d8d1a8eff39c1f2b390e8c
sollertia <sollertia331@gmail.com>
2021-10-26 12:18:19 +0900

    Initial Setting

---
+++ src/main/java/com/sollertia/habit/HabitApplication.java
@@ -0,0 +1,13 @@
+package com.sollertia.habit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitApplication.class, args);
+    }
+
+}
