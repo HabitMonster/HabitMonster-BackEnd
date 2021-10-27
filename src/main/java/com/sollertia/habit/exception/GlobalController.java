7258298fa57a26c83e5513b35bbe5d1d248d56b3
rockintuna <rockintuna@naver.com>
2021-10-27 18:32:33 +0900

    Oauth2Google: Provider Missmatch 예외 처리 추가

--- src/main/java/com/sollertia/habit/exception/GlobalController.java
+++ src/main/java/com/sollertia/habit/exception/GlobalController.java
@@ -1,8 +1,16 @@
 package com.sollertia.habit.exception;
 
+import com.sollertia.habit.utils.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalController {
 
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponseDto> oAuthProviderMissMatchExceptionHandler(OAuthProviderMissMatchException exception) {
+        return new ResponseEntity<>(ErrorResponseDto.badRequest(exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
 }
