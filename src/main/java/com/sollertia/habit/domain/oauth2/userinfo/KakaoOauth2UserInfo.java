262eeb130d4eee535a181a1fd8be94439c048ba7
rockintuna <rockintuna@naver.com>
2021-10-27 22:13:21 +0900

    Oauth2Google: KakaoOauth2UserInfo 추가

---
+++ src/main/java/com/sollertia/habit/domain/oauth2/userinfo/KakaoOauth2UserInfo.java
@@ -0,0 +1,38 @@
+package com.sollertia.habit.domain.oauth2.userinfo;
+
+import com.sollertia.habit.domain.user.ProviderType;
+
+import java.util.Map;
+
+public class KakaoOauth2UserInfo extends Oauth2UserInfo {
+
+    public KakaoOauth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getName() {
+        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+
+        if (properties == null) {
+            return null;
+        }
+
+        return (String) properties.get("nickname");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("account_email");
+    }
+
+    @Override
+    public ProviderType getProviderType() {
+        return ProviderType.KAKAO;
+    }
+}
