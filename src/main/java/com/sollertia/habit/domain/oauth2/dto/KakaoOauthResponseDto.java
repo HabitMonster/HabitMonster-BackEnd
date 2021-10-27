5b90289e7f6c45d3405690e22e085e77fecd18b0
rockintuna <rockintuna@naver.com>
2021-10-27 22:14:10 +0900

    Oauth2Google: KakaoOauthResponseDto 추가

---
+++ src/main/java/com/sollertia/habit/domain/oauth2/dto/KakaoOauthResponseDto.java
@@ -0,0 +1,14 @@
+package com.sollertia.habit.domain.oauth2.dto;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+public class KakaoOauthResponseDto {
+    private String token_type;
+    private String access_token;
+    private String expires_in;
+    private String refresh_token;
+    private String refresh_token_expires_in;
+    private String scope;
+}
