52ec37459e4738160afc89840a33346ab0eccd88
rockintuna <rockintuna@naver.com>
2021-10-27 18:41:19 +0900

    Oauth2Google: Oauth2Service 인터페이스 추가

---
+++ src/main/java/com/sollertia/habit/domain/user/oauth2/Oauth2Service.java
@@ -0,0 +1,8 @@
+package com.sollertia.habit.domain.user.oauth2;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sollertia.habit.domain.user.oauth2.userinfo.Oauth2UserInfo;
+
+public interface Oauth2Service {
+    Oauth2UserInfo getUserInfoByCode(String authCode) throws JsonProcessingException;
+}
