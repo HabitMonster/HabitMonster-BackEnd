59f68e1deecc11649e0dedc73b86b528bc68b83f
rockintuna <rockintuna@naver.com>
2021-10-28 17:06:41 +0900

    Oauth2Naver: naver state 전달 안됨 예외 처리 추가

---
+++ src/main/java/com/sollertia/habit/exception/NaverOauth2Exception.java
@@ -0,0 +1,7 @@
+package com.sollertia.habit.exception;
+
+public class NaverOauth2Exception extends RuntimeException {
+    public NaverOauth2Exception(String message) {
+        super(message);
+    }
+}
