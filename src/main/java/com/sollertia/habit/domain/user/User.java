36d070b24204f4537be2014a28b9c6fc9444472a
smallzoo-dev <joongyue@gmail.com>
2021-10-28 16:00:55 +0900

    ServiceDevelop: user tnwjd

--- src/main/java/com/sollertia/habit/domain/user/User.java
+++ src/main/java/com/sollertia/habit/domain/user/User.java
@@ -4,10 +4,15 @@ import com.sollertia.habit.domain.avatar.Avatar;
 import com.sollertia.habit.domain.avatar.AvatarCollection;
 import com.sollertia.habit.domain.habit.habitCounter.HabitWithCounter;
 import com.sollertia.habit.domain.habit.enums.Level;
+import com.sollertia.habit.domain.habit.habitTimer.HabitWithTimer;
+import com.sollertia.habit.domain.oauth2.userinfo.Oauth2UserInfo;
 import com.sollertia.habit.domain.userteam.UserTeam;
+import lombok.Getter;
+
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Getter
