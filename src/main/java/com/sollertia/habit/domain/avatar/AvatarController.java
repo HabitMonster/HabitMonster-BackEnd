36bf966253917739af61e17c4e41b7bbb98c2fa4
rockintuna <rockintuna@naver.com>
2021-10-29 22:52:12 +0900

    AvatarService: 아바타 컨트롤러 Swagger 이름

--- src/main/java/com/sollertia/habit/domain/avatar/AvatarController.java
+++ src/main/java/com/sollertia/habit/domain/avatar/AvatarController.java
@@ -4,6 +4,7 @@ import com.sollertia.habit.domain.avatar.dto.AvatarListResponseDto;
 import com.sollertia.habit.domain.avatar.dto.AvatarResponseDto;
 import com.sollertia.habit.domain.avatar.dto.AvatarSelectRequestDto;
 import com.sollertia.habit.domain.user.UserDetailsImpl;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +18,13 @@ public class AvatarController {
 
     private final AvatarService avatarService;
 
+    @ApiOperation(value = "LV1 아바타 목록 조회")
     @GetMapping("/avatars")
     public AvatarListResponseDto getAllAvatars() {
         return avatarService.getAllAvatars();
     }
 
+    @ApiOperation(value = "아바타 변경")
     @PostMapping("/avatars/select")
     public AvatarResponseDto selectAvatar(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
