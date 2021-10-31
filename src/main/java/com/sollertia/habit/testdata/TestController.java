333dbffcb2ccac1cbf9c470b16c38ff08fdf7caf
sollertia <sollertia331@gmail.com>
2021-10-31 22:02:22 +0900

    TestData,Controller -> Test 용 user 추가 및 testUser의 토큰발급 testController 추가

--- src/main/java/com/sollertia/habit/testdata/TestController.java
+++ src/main/java/com/sollertia/habit/testdata/TestController.java
@@ -1,13 +1,16 @@
-package com.sollertia.habit.config.jwt;
+package com.sollertia.habit.testdata;
 
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String login() {
         return "index.html";
     }
---
+++ src/main/java/com/sollertia/habit/testdata/TestToken.java
@@ -0,0 +1,28 @@
+package com.sollertia.habit.testdata;
+
+import com.sollertia.habit.config.jwt.JwtTokenProvider;
+import com.sollertia.habit.config.jwt.dto.JwtResponseDto;
+import com.sollertia.habit.domain.user.User;
+import com.sollertia.habit.domain.user.UserRepository;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequiredArgsConstructor
+@RestController
+public class TestToken {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+    @ApiOperation("test 토큰 발급")
+    @GetMapping("/test/token")
+    public JwtResponseDto testToken(){
+
+        User testUser = userRepository.findBySocialId("1234G").orElseThrow(()->
+                new IllegalArgumentException("유저 없음"));
+
+        String accessToken = jwtTokenProvider.responseAccessToken(testUser);
+        String refreshToken = jwtTokenProvider.responseRefreshToken(testUser);
+
+        return JwtResponseDto.builder().accesstoken(accessToken).refreshtoken(refreshToken).build();
+    }
+}
