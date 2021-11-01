62fc4c4f51e6acd7c17bb08f378fcc84dc7786c8
rockintuna <rockintuna@naver.com>
2021-11-01 17:51:00 +0900

    MainResponseFix: HabitSummaryVo 필드명 변경 (progressPercentage => achievePercentage)

--- src/main/java/com/sollertia/habit/domain/habit/dto/HabitSummaryVo.java
+++ src/main/java/com/sollertia/habit/domain/habit/dto/HabitSummaryVo.java
@@ -19,7 +19,7 @@ public class HabitSummaryVo {
     private Long count;
     private Long current;
     private Long sessionDuration;
-    private Long progressPercentage;
+    private Long achievePercentage;
     private Category category;
 
     public static List<HabitSummaryVo> listOf(List<HabitWithCounter> habits) {
@@ -39,7 +39,7 @@ public class HabitSummaryVo {
                 .count(habit.getGoalCountInSession())
                 .current(habit.getCurrent())
                 .sessionDuration(habit.getSessionDuration())
-                .progressPercentage(habit.getAchievePercentage())
+                .achievePercentage(habit.getAchievePercentage())
                 .category(habit.getCategory())
                 .build();
     }
