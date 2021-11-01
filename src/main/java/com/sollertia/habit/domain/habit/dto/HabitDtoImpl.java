aca46365e7723c3964d5d80fcc93e3365d42377d
smallzoo-dev <joongyue@gmail.com>
2021-11-01 14:58:18 +0900

    HabitFix: Fix: 필요 없는 enum,dto 필드 제거

--- src/main/java/com/sollertia/habit/domain/habit/dto/HabitDtoImpl.java
+++ src/main/java/com/sollertia/habit/domain/habit/dto/HabitDtoImpl.java
@@ -1,12 +1,9 @@
 package com.sollertia.habit.domain.habit.dto;
 
-import com.sollertia.habit.domain.habit.enums.Day;
 import com.sollertia.habit.domain.category.enums.Category;
 import com.sollertia.habit.domain.team.Team;
 import com.sollertia.habit.domain.user.User;
 import lombok.*;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -22,7 +19,6 @@ public class HabitDtoImpl {
     private String durationStart;
     private String durationEnd;
     private String category;
-    private String dayList;
     private Long count;
     private Long goalTime = 0l;
     private String practiseDays;
