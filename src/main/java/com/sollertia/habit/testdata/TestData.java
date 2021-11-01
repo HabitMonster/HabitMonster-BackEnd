570802879d56914845f0239e3b8c8e2478794df6
rockintuna <rockintuna@naver.com>
2021-11-01 15:36:36 +0900

    MainPage: TestData, 테스트용 몬스터 추가

--- src/main/java/com/sollertia/habit/testdata/TestData.java
+++ src/main/java/com/sollertia/habit/testdata/TestData.java
@@ -1,5 +1,8 @@
 package com.sollertia.habit.testdata;
 
+import com.sollertia.habit.domain.monster.EvolutionGrade;
+import com.sollertia.habit.domain.monster.Monster;
+import com.sollertia.habit.domain.monster.MonsterRepository;
 import com.sollertia.habit.domain.user.User;
 import com.sollertia.habit.domain.user.UserRepository;
 import com.sollertia.habit.domain.user.UserType;
@@ -14,11 +17,15 @@ import org.springframework.stereotype.Component;
 public class TestData implements ApplicationRunner {
 
     private final UserRepository userRepository;
-
+    private final MonsterRepository monsterRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        Monster monster = new Monster(1L, "cat", EvolutionGrade.EV1, "cat.img");
+        monsterRepository.save(monster);
+
         User testUser = User.builder().socialId("1234G").type(UserType.Google).build();
+        testUser.updateMonster(monster, "test monster");
         userRepository.save(testUser);
     }
 }
