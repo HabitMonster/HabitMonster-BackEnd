330981c98cf755b68602fff3b85a37369075080f
rockintuna <rockintuna@naver.com>
2021-11-02 16:08:50 +0900

    FixMonsterAndMain: 몬스터 조회 응답 객체 변경

--- src/main/java/com/sollertia/habit/domain/monster/MonsterService.java
+++ src/main/java/com/sollertia/habit/domain/monster/MonsterService.java
@@ -1,9 +1,6 @@
 package com.sollertia.habit.domain.monster;
 
-import com.sollertia.habit.domain.monster.dto.MonsterListResponseDto;
-import com.sollertia.habit.domain.monster.dto.MonsterResponseDto;
-import com.sollertia.habit.domain.monster.dto.MonsterSelectRequestDto;
-import com.sollertia.habit.domain.monster.dto.MonsterVo;
+import com.sollertia.habit.domain.monster.dto.*;
 import com.sollertia.habit.domain.user.User;
 import com.sollertia.habit.domain.user.UserRepository;
 import com.sollertia.habit.exception.MonsterNotFoundException;
@@ -31,7 +28,7 @@ public class MonsterService {
             monsters.remove(user.getMonster());
         }
         return MonsterListResponseDto.builder()
-                .monsters(monsters)
+                .monsters(MonsterSummaryVo.listFromMonsterList(monsters))
                 .responseMessage("LV1 몬스터를 불러오는데 성공했습니다.")
                 .statusCode(200)
                 .build();
@@ -61,12 +58,8 @@ public class MonsterService {
 
     public MonsterListResponseDto getMonsterCollection(User user) {
         List<MonsterCollection> monsterCollectionList = monsterCollectionRepository.findAllByUser(user);
-        List<Monster> monsters = new ArrayList<>();
-        for (MonsterCollection monsterCollection : monsterCollectionList) {
-            monsters.add(monsterCollection.getMonster());
-        }
         return MonsterListResponseDto.builder()
-                .monsters(monsters)
+                .monsters(MonsterSummaryVo.listFromCollectionList(monsterCollectionList))
                 .responseMessage("몬스터 컬렉션 조회 성공")
                 .statusCode(200)
                 .build();
