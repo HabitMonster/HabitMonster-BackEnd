a12cb831375d8341febdcfd305266069e23fc47b
rockintuna <rockintuna@naver.com>
2021-11-04 14:07:06 +0900

    FixMonsterVo: MonsterVo level 추가

--- src/main/java/com/sollertia/habit/domain/monster/EvolutionGrade.java
+++ src/main/java/com/sollertia/habit/domain/monster/EvolutionGrade.java
@@ -1,5 +1,15 @@
 package com.sollertia.habit.domain.monster;
 
 public enum EvolutionGrade {
-    EV1,EV2,EV3,EV4,EV5
+    EV1(1),EV2(2),EV3(3),EV4(4),EV5(5);
+
+    private final Integer value;
+
+    EvolutionGrade(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
 }
--- src/main/java/com/sollertia/habit/domain/monster/dto/MonsterVo.java
+++ src/main/java/com/sollertia/habit/domain/monster/dto/MonsterVo.java
@@ -10,12 +10,14 @@ public class MonsterVo {
     private Long monsterId;
     private String monsterImage;
     private String monsterName;
+    private Integer level;
 
     public static MonsterVo of(Monster monster, String name) {
         return MonsterVo.builder()
                 .monsterId(monster.getId())
                 .monsterImage(monster.getImageUrl())
                 .monsterName(name)
+                .level(monster.getGrade().getValue())
                 .build();
     }
 }
