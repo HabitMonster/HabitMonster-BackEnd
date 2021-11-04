1ff44db1a8084df6db2d4ba59af05d07de3bda92
rockintuna <rockintuna@naver.com>
2021-11-04 13:50:25 +0900

    RestTemplateBean: @RestClientTest 어노테이션 사용

--- src/test/java/com/sollertia/habit/domain/oauth2/loginutil/GoogleSocialLoginUtilTest.java
+++ src/test/java/com/sollertia/habit/domain/oauth2/loginutil/GoogleSocialLoginUtilTest.java
@@ -1,29 +1,56 @@
 package com.sollertia.habit.domain.oauth2.loginutil;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sollertia.habit.domain.oauth2.userinfo.Oauth2UserInfo;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @ExtendWith(MockitoExtension.class)
+@RestClientTest(GoogleSocialLoginUtil.class)
 class GoogleSocialLoginUtilTest {
 
-    @InjectMocks
+    @Autowired
+    private MockRestServiceServer server;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
     private GoogleSocialLoginUtil googleSocialLoginUtil;
 
-    @Mock
-    private RestTemplate restTemplate;
-
     String authCode;
+    Map<String, Object> attributes;
 
     @BeforeEach
-    private void before() {
+    public void setUp() throws Exception {
         authCode = "abcdefg1234567";
+        attributes = new HashMap<>();
+        attributes.put("sub", "123456789");
+        attributes.put("name", "tester");
+        attributes.put("email", "tester.test.com");
+        String detailsString =
+                objectMapper.writeValueAsString(attributes);
+
+        this.server.expect(requestTo(GoogleSocialLoginUtil.GOOGLE_TOKEN_INFO_URL
+                        +"?id_token="+authCode))
+                .andRespond(withSuccess(detailsString, MediaType.APPLICATION_JSON));
     }
 
     @Test
@@ -31,8 +58,11 @@ class GoogleSocialLoginUtilTest {
         //given
 
         //when
-        googleSocialLoginUtil.getUserInfoByCode(authCode);
+        Oauth2UserInfo userInfo = googleSocialLoginUtil.getUserInfoByCode(authCode);
 
         //then
+        assertThat(userInfo.getId()).isEqualTo(attributes.get("sub")+"G");
+        assertThat(userInfo.getName()).isEqualTo(attributes.get("name"));
+        assertThat(userInfo.getEmail()).isEqualTo(attributes.get("email"));
     }
 }
