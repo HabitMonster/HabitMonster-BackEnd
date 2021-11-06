eafdbf752f28d6b49601d02ef4120e6896487e07
sollertia <sollertia331@gmail.com>
2021-11-06 16:56:59 +0900

    JwtTest: 중간 commit

--- src/test/java/com/sollertia/habit/config/jwt/JwtControllerTest.java
+++ src/test/java/com/sollertia/habit/config/jwt/JwtControllerTest.java
@@ -1,5 +1,7 @@
 package com.sollertia.habit.config.jwt;
 
+import com.sollertia.habit.domain.monster.dto.MonsterListResponseDto;
+import com.sollertia.habit.domain.monster.dto.MonsterSummaryVo;
 import com.sollertia.habit.domain.oauth2.userinfo.GoogleOauth2UserInfo;
 import com.sollertia.habit.domain.oauth2.userinfo.Oauth2UserInfo;
 import com.sollertia.habit.domain.user.User;
@@ -11,12 +13,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = JwtController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -34,6 +52,23 @@ class JwtControllerTest {
     User testUser;
     UserDetailsImpl mockUserDetails;
     SecurityContext securityContext;
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    public static final String signatureToken =
+            "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0RyIsInR5cGUiOiJHb29nbGUiLCJpYXQiOjE2MzYwMjQ4NzcsImV4cCI6MTYzNjAyNDg4Mn0.OAXTkojY2evdc-NsU2Za0Bv6VaWsv0FQVlhSt7QhxbY";
+
+    public static final String expiredToken =
+            "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODlHIiwidHlwZSI6Ikdvb2dsZSIsImlhdCI6MTYzNjAzNTgyNiwiZXhwIjoxNjM2MDM1ODI3fQ.zI4KZQPw_L8yEH4XdsKYMF5ZDaY5k-IzFYkWA9wEgas";
+
+    private static final String successToken =
+            "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODlHIiwidHlwZSI6Ikdvb2dsZSIsImlhdCI6MTYzNjAzNTgyNiwiZXhwIjoxODg2MzYyODY2fQ.qHqotXvz0vipsiQmHiS3utO-43yop98fdUK9XcMXXWQ";
+
+    private static final String MalformedToken = "abcdefg";
+
+    private void authenticated() {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(mockUserDetails, "", mockUserDetails.getAuthorities());
+        securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+    }
 
     @BeforeEach
     private void beforeEach() {
@@ -47,6 +82,22 @@ class JwtControllerTest {
     }
 
     @Test
-    void loginCheck() {
+    void loginCheck() throws Exception {
+        //given
+        authenticated();
+        given(jwtTokenProvider.requestRefreshToken(request)).willReturn(successToken);
+        //when
+        mvc.perform(post("/user/loginCheck")
+                        .with(csrf()))
+
+
+                //then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.monsters[0].monsterId").value("1"))
+                .andExpect(jsonPath("$.monsters[0].monsterImage").value("monster.img"))
+                .andExpect(jsonPath("$.responseMessage").value("LV1 몬스터를 불러오는데 성공했습니다."))
+                .andExpect(jsonPath("$.statusCode").value("200"));
+
+       // verify().(testUser);
     }
 }
