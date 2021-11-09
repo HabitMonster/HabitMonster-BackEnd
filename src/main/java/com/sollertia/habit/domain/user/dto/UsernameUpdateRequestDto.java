0dbd3e32f050bc36d3eb51f83b267d27573c6578
sollertia <sollertia331@gmail.com>
2021-11-09 19:29:05 +0900

    User: user 닉네임 valid 처리, TimeStamped 상속

--- src/main/java/com/sollertia/habit/domain/user/dto/UsernameUpdateRequestDto.java
+++ src/main/java/com/sollertia/habit/domain/user/dto/UsernameUpdateRequestDto.java
@@ -3,8 +3,15 @@ package com.sollertia.habit.domain.user.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @NoArgsConstructor
 public class UsernameUpdateRequestDto {
+    @NotBlank(message = "값이 비어있지 않아야 합니다.")
+    @Size(max = 10, message = "닉네임은 최대 10자입니다.")
+    @Pattern(regexp = "^[a-zA-Z가-힣0-9]*$",message = "닉네임은 알파벳대소문자, 한글, 숫자만 가능합니다.")
     private String username;
 }
