6c544315d8b126ba3e372fa3b6cde8ee892c1d73
rockintuna <rockintuna@naver.com>
2021-11-11 19:37:33 +0900

    JasyptProperties: JasyptConfig 디폴트 미적용으로 수정

--- src/main/java/com/sollertia/habit/global/config/JasyptConfig.java
+++ src/main/java/com/sollertia/habit/global/config/JasyptConfig.java
@@ -19,6 +19,11 @@ public class JasyptConfig {
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
         config.setPassword(password);
         config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
         encryptor.setConfig(config);
         return encryptor;
     }
