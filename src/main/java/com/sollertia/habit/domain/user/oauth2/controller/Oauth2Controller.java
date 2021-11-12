439b8b85af8b91ebd4dc2efd5957d915020a118b
rockintuna <rockintuna@naver.com>
2021-11-13 05:52:32 +0900

    LoginPost: 로그인 API POST 추가

--- src/main/java/com/sollertia/habit/domain/user/oauth2/controller/Oauth2Controller.java
+++ src/main/java/com/sollertia/habit/domain/user/oauth2/controller/Oauth2Controller.java
@@ -1,5 +1,6 @@
 package com.sollertia.habit.domain.user.oauth2.controller;
 
+import com.sollertia.habit.domain.user.oauth2.dto.CodeDto;
 import com.sollertia.habit.domain.user.oauth2.service.Oauth2UserService;
 import com.sollertia.habit.domain.user.oauth2.service.SocialLoginService;
 import com.sollertia.habit.domain.user.oauth2.userinfo.Oauth2UserInfo;
@@ -9,10 +10,7 @@ import com.sollertia.habit.domain.user.entity.User;
 import com.sollertia.habit.global.utils.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,6 +26,28 @@ public class Oauth2Controller {
                                                 @PathVariable String socialName) {
 
         Oauth2UserInfo userInfo = socialLoginService.getUserInfo(socialName, authCode);
+        userInfo = oauth2UserService.putUserInto(userInfo);
+        User user = userInfo.getUser();
+
+        String accessToken =  jwtTokenProvider.responseAccessToken(user);
+        String refreshToken = jwtTokenProvider.responseRefreshToken(user);
+        redisUtil.setDataExpire(refreshToken, user.getSocialId(), JwtTokenProvider.REFRESH_TOKEN_USETIME / 1000L);
+
+        return JwtResponseDto.builder()
+                .accessToken(accessToken)
+                .createdAt(user.getCreatedAt().toLocalDate().toString())
+                .isFirstLogin(userInfo.isFirstLogin())
+                .refreshToken(refreshToken)
+                .statusCode(200)
+                .responseMessage("Issuance completed Token")
+                .build();
+    }
+
+    @PostMapping("/user/login/{socialName}")
+    public JwtResponseDto login(@RequestBody CodeDto authCode,
+                                @PathVariable String socialName) {
+
+        Oauth2UserInfo userInfo = socialLoginService.getUserInfo(socialName, authCode.getCode());
         userInfo = oauth2UserService.putUserInto(userInfo);
         User user = userInfo.getUser();
 
