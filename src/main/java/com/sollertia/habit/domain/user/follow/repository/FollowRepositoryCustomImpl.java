8b87f4661cc1b07a163dc2147873a0c172cdb811
sollertia <sollertia331@gmail.com>
2021-11-17 17:01:15 +0900

    QueryDsl: Repository Custom 준비

---
+++ src/main/java/com/sollertia/habit/domain/user/follow/repository/FollowRepositoryCustomImpl.java
@@ -0,0 +1,20 @@
+package com.sollertia.habit.domain.user.follow.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sollertia.habit.domain.user.follow.dto.FollowVo;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class FollowRepositoryCustomImpl implements FollowRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<FollowVo> followers(Long followerId) {
+        return null;
+        //return queryFactory.select(Projections.class,);
+    }
+}
