b69ad661924d599f5adec2262a05bc8f4199479b
sollertia <sollertia331@gmail.com>
2021-11-20 02:33:38 +0900

    CategoryCaching: 완료

--- src/main/java/com/sollertia/habit/domain/category/controller/CategoryController.java
+++ src/main/java/com/sollertia/habit/domain/category/controller/CategoryController.java
@@ -4,19 +4,19 @@ import com.sollertia.habit.domain.category.dto.CategoryResponseDto;
 import com.sollertia.habit.domain.category.dto.CategoryVo;
 import com.sollertia.habit.domain.category.enums.Category;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.annotations.Cacheable;
 
 import java.util.List;
 
 @RestController
 public class CategoryController {
 
-    //@Cacheable()
+    @Cacheable(value = "Categories", key = "1")
     @ApiOperation(value = "Category 목록 조회")
     @GetMapping("/categories")
-    public CategoryResponseDto categoryPresetList(){
+    public CategoryResponseDto categoryPresetList() {
         List<CategoryVo> list = Category.getCategories();
         return CategoryResponseDto.builder().categories(list).statusCode(200).responseMessage("Category Query Completed").build();
     }
