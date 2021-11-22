7cb649f869e6f4029d1776645c4e252f78e5478d
rockintuna <rockintuna@naver.com>
2021-11-23 00:05:27 +0900

    Revert "RollbackUserHabit: GET /user/habits 롤백 (습관 수 제거)"
    This reverts commit 77855a214b3d8d46951e75a024101f2e8c72e3b7.

--- src/main/java/com/sollertia/habit/domain/habit/dto/HabitSummaryListResponseDto.java
+++ src/main/java/com/sollertia/habit/domain/habit/dto/HabitSummaryListResponseDto.java
@@ -11,4 +11,5 @@ import java.util.List;
 @SuperBuilder
 public class HabitSummaryListResponseDto extends DefaultResponseDto {
     private List<HabitSummaryVo> habits;
+    private Integer totalHabitCount;
 }
