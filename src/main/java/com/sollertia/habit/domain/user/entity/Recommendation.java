c3e44025b3055313fd74209144f666733566d017
rockintuna <rockintuna@naver.com>
2021-11-23 14:21:11 +0900

    RecommendUserLogic: 추천 사용자 생성메서드 추가

--- src/main/java/com/sollertia/habit/domain/user/entity/Recommendation.java
+++ src/main/java/com/sollertia/habit/domain/user/entity/Recommendation.java
@@ -1,14 +1,18 @@
 package com.sollertia.habit.domain.user.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.sollertia.habit.domain.user.entity.User;
 import com.sollertia.habit.domain.user.enums.RecommendationType;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Recommendation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +28,33 @@ public class Recommendation {
     @JoinColumn(name = "user_id", nullable = false)
     @JsonIgnore
     private User user;
+
+    private void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    private void setType(RecommendationType type) {
+        this.type = type;
+    }
+
+    private void setUser(User user) {
+        this.user = user;
+    }
+
+    private static Recommendation of(User user, RecommendationType type) {
+        Recommendation recommendation = new Recommendation();
+        recommendation.setUser(user);
+        recommendation.setType(type);
+        return recommendation;
+    }
+
+    private static List<Recommendation> listOf(List<User> userList, RecommendationType type) {
+        List<Recommendation> recommendationList = new ArrayList<>();
+        for (int i = 0; i < userList.size(); i++) {
+            Recommendation recommendation = Recommendation.of(userList.get(i), type);
+            recommendation.setNumber(i);
+            recommendationList.add(recommendation);
+        }
+        return recommendationList;
+    }
 }
