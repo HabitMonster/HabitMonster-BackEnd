24965822a0444f9ea27927393acef0718b175b1e
rockintuna <rockintuna@naver.com>
2021-11-23 09:48:11 +0900

    RecommendationDirectory: 추천 사용자 관련 디렉토리 이동

--- src/main/java/com/sollertia/habit/domain/user/repository/RecommendationRepository.java
+++ src/main/java/com/sollertia/habit/domain/user/repository/RecommendationRepository.java
@@ -1,6 +1,6 @@
-package com.sollertia.habit.domain.user.recommendation.repository;
+package com.sollertia.habit.domain.user.repository;
 
-import com.sollertia.habit.domain.user.recommendation.entity.Recommendation;
+import com.sollertia.habit.domain.user.entity.Recommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
