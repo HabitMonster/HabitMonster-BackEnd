e0bdb3ee1e406dd609ddc91ef326a0a08bac7b3d
rockintuna <rockintuna@naver.com>
2021-11-23 19:44:36 +0900

    RecommendedUserScheduler: 어노테이션 정리

--- src/main/java/com/sollertia/habit/domain/user/repository/RecommendationRepositoryImpl.java
+++ src/main/java/com/sollertia/habit/domain/user/repository/RecommendationRepositoryImpl.java
@@ -1,23 +1,18 @@
 package com.sollertia.habit.domain.user.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sollertia.habit.domain.user.entity.Recommendation;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import static com.sollertia.habit.domain.user.entity.QRecommendation.recommendation;
 import static com.sollertia.habit.domain.user.entity.QUser.user;
 
-import com.sollertia.habit.domain.user.entity.Recommendation;
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
-import java.util.List;
-
-@Repository
+@RequiredArgsConstructor
 public class RecommendationRepositoryImpl implements RecommendationRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
-
-    public RecommendationRepositoryImpl(EntityManager em) {
-        this.queryFactory = new JPAQueryFactory(em);
-    }
 
     @Override
     public List<Recommendation> searchByNumber(int number) {
--- src/main/java/com/sollertia/habit/domain/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/sollertia/habit/domain/user/repository/UserRepositoryImpl.java
@@ -4,23 +4,18 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sollertia.habit.domain.category.enums.Category;
 import com.sollertia.habit.domain.user.entity.User;
-import org.springframework.stereotype.Repository;
+import lombok.RequiredArgsConstructor;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
 import static com.sollertia.habit.domain.completedhabbit.entity.QCompletedHabit.completedHabit;
 import static com.sollertia.habit.domain.user.entity.QUser.user;
 import static com.sollertia.habit.domain.user.follow.entity.QFollow.follow;
 
-@Repository
+@RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepositoryCustom{
 
     private final JPAQueryFactory queryFactory;
-
-    public UserRepositoryImpl(EntityManager em) {
-        this.queryFactory = new JPAQueryFactory(em);
-    }
 
     @Override
     public List<User> searchTop10ByCategory(Category category) {
