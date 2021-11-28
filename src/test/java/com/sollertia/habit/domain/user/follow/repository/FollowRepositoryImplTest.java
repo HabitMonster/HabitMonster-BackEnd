d941c0308ae4de7d93a508514a7c64b28b6661c7
sollertia <sollertia331@gmail.com>
2021-11-29 08:06:48 +0900

    FollowRepositoryImpl: Test 코드 완료

--- src/test/java/com/sollertia/habit/domain/user/follow/repository/FollowRepositoryImplTest.java
+++ src/test/java/com/sollertia/habit/domain/user/follow/repository/FollowRepositoryImplTest.java
@@ -43,10 +43,16 @@ class FollowRepositoryImplTest {
     private UserRepository userRepository;
     @Autowired
     private MonsterDatabaseRepository monsterDatabaseRepository;
+    @Autowired
+    EntityManager em;
 
     User testUser;
     User testUser2;
     Monster monster;
+    Monster monster2;
+    Follow follow1;
+    Follow follow2;
+
 
     @BeforeEach
     private void beforeEach() {
@@ -60,9 +66,9 @@ class FollowRepositoryImplTest {
         monsterDatabaseRepository.save(monsterDatabase1);
         monster = Monster.createNewMonster("blue", monsterDatabase1);
         testUser.updateMonster(monster);
-        Whitebox.setInternalState(testUser, "monsterCode", "monsterCode");
-        Whitebox.setInternalState(testUser, "id", 1L);
-        userRepository.save(testUser);
+        testUser.setMonsterCode("monsterCode");
+        testUser = userRepository.save(testUser);
+        em.flush();
 
         attributes.clear();
         attributes.put("sub", "1234");
@@ -70,34 +76,95 @@ class FollowRepositoryImplTest {
         attributes.put("email", "tester2.test.com");
         Oauth2UserInfo oauth2UserInfo2 = new GoogleOauth2UserInfo(attributes);
         testUser2 = User.create(oauth2UserInfo2);
-        testUser2.updateMonster(monster);
+        MonsterDatabase monsterDatabase2 = new MonsterDatabase(Level.LV1, MonsterType.RED, "red.img");
+        monsterDatabaseRepository.save(monsterDatabase2);
+        monster2 = Monster.createNewMonster("red", monsterDatabase2);
+        testUser2.updateMonster(monster2);
         testUser2.setMonsterCode("monsterCode2");
-        Whitebox.setInternalState(testUser2, "monsterCode", "monsterCode2");
-        Whitebox.setInternalState(testUser2, "id", 2L);
-        userRepository.save(testUser2);
+        testUser2 = userRepository.save(testUser2);
+        em.flush();
+
+        follow1 = Follow.create(testUser2, testUser);
+        follow1 = followRepository.save(follow1);
+        em.flush();
+        follow2 = Follow.create(testUser, testUser2);
+        follow2 = followRepository.save(follow2);
     }
 
     @DisplayName("사용자의 followers 가져오기")
     @Test
     void searchFollowersByUser(){
 
-        Follow follow = Follow.create(testUser2, testUser);
-        Whitebox.setInternalState(follow, "id", 1L);
-        follow = followRepository.save(follow);
-
-        assertThat(follow.getFollower().getId()).isEqualTo(testUser2.getId());
-        assertThat(follow.getFollowing().getId()).isEqualTo(testUser.getId());
-
         List<FollowDto> followDtoList = followRepository.searchFollowersByUser(testUser);
 
         assertThat(followDtoList.get(0).getMonsterCode()).isEqualTo(testUser2.getMonsterCode());
-        assertThat(followDtoList.get(0).getMonsterCode()).isEqualTo(testUser2.getMonsterCode());
-        assertThat(followDtoList.get(0).getMonsterCode()).isEqualTo(testUser2.getMonsterCode());
-        assertThat(followDtoList.get(0).getMonsterCode()).isEqualTo(testUser2.getMonsterCode());
-        assertThat(followDtoList.get(0).getMonsterCode()).isEqualTo(testUser2.getMonsterCode());
-        assertThat(followDtoList.get(0).getMonsterCode()).isEqualTo(testUser2.getMonsterCode());
-        assertThat(followDtoList.get(0).getMonsterCode()).isEqualTo(testUser2.getMonsterCode());
+        assertThat(followDtoList.get(0).getNickName()).isEqualTo(testUser2.getUsername());
+        assertThat(followDtoList.get(0).getIsFollowed()).isEqualTo(true);
     }
+
+    @DisplayName("특정 사용자의 followers 가져오기")
+    @Test
+    void searchFollowersBySelectUser(){
+
+        List<FollowDto> followDtoList = followRepository.searchFollowersByUser(testUser, testUser2);
+
+        assertThat(followDtoList.get(0).getMonsterCode()).isEqualTo(testUser.getMonsterCode());
+        assertThat(followDtoList.get(0).getNickName()).isEqualTo(testUser.getUsername());
+        assertThat(followDtoList.get(0).getIsFollowed()).isEqualTo(null);
+    }
+
+    @DisplayName("사용자의 followings 가져오기")
+    @Test
+    void searchFollowingsByUser(){
+
+        List<FollowDto> followDtoList = followRepository.searchFollowingsByUser(testUser);
+
+        assertThat(followDtoList.get(0).getMonsterCode()).isEqualTo(testUser2.getMonsterCode());
+        assertThat(followDtoList.get(0).getNickName()).isEqualTo(testUser2.getUsername());
+        assertThat(followDtoList.get(0).getIsFollowed()).isEqualTo(true);
+    }
+    @DisplayName("특정 사용자의 followings 가져오기")
+    @Test
+    void searchFollowingsSelectByUser(){
+
+        List<FollowDto> followDtoList = followRepository.searchFollowingsByUser(testUser, testUser2);
+
+        assertThat(followDtoList.get(0).getMonsterCode()).isEqualTo(testUser.getMonsterCode());
+        assertThat(followDtoList.get(0).getNickName()).isEqualTo(testUser.getUsername());
+        assertThat(followDtoList.get(0).getIsFollowed()).isEqualTo(null);
+    }
+
+    @DisplayName("사용자 검색")
+    @Test
+    void searchUser(){
+
+        FollowDto followDto = followRepository.searchUser("monsterCode", testUser);
+
+        assertThat(followDto.getMonsterCode()).isEqualTo(testUser.getMonsterCode());
+        assertThat(followDto.getNickName()).isEqualTo(testUser.getUsername());
+        assertThat(followDto.getIsFollowed()).isEqualTo(null);
+    }
+
+    @DisplayName("Following 수")
+    @Test
+    void countByFollower(){
+
+        Long number = followRepository.countByFollower(testUser);
+
+        assertThat(number).isEqualTo(1L);
+    }
+
+    @DisplayName("Follower 수")
+    @Test
+    void countByFollowing(){
+
+        Long number = followRepository.countByFollowing(testUser);
+
+        assertThat(number).isEqualTo(1L);
+    }
+
+
+
 
 
 }
