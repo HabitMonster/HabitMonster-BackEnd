19b23efeb8e01746f9fb0681915d6a81ad557a46
sollertia <sollertia331@gmail.com>
2021-11-29 22:35:20 +0900

    TestCode: Filter 넘어가게 해주는 설정 추가 및 JwtToken Null 처리 분기 Test코드 추가

--- src/test/java/com/sollertia/habit/domain/notice/controller/NoticeControllerTest.java
+++ src/test/java/com/sollertia/habit/domain/notice/controller/NoticeControllerTest.java
@@ -12,6 +12,7 @@ import com.sollertia.habit.global.utils.RedisUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -34,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = NoticeController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class NoticeControllerTest {
 
     @Autowired
--- src/test/java/com/sollertia/habit/domain/user/follow/controller/FollowControllerTest.java
+++ src/test/java/com/sollertia/habit/domain/user/follow/controller/FollowControllerTest.java
@@ -22,6 +22,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -44,6 +45,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = FollowController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @RunWith(PowerMockRunner.class)
 class FollowControllerTest {
 
